25ba2c03c78104b33001a915c254b018e391fcf6
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-08-16 19:28:29 +0200

    Split .bashrc setup like on Fedora
    It seems that the structure of .bashrc is quite different on Fedora:
    it has not provision for skipping setup on non-interactive shelss,
    and pulls in /etc/bashrc. And it has support for ~/.bashrc.d/.
    So let's move things into files under ~/.bashrc.d/, keep original
    .bashrc on Fedora, and add support for ~/.bashrc.d/ into the
    version of .bashrc that is installed on Debian/Ubuntu.
    
    [I think the approach of modifying $PATH in .bashrc is broken:
    we would need to do this for every shell and for non-shell
    environments. This should be a systemd environment generator
    instead.]

---
+++ files/bashrc.d/50-history.sh
@@ -0,0 +1,27 @@
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=10000
+HISTFILESIZE=20000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+shopt -s globstar
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
